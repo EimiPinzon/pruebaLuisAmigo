31fb05553b208dc329dd30c5495441afa35734b2
Eimi <eimipinzonhoyos@gmail.com>
2021-07-17 16:40:04 -0500

    actualización

---
+++ pruebatecnica/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <h1>prueba tecnica</h1>
+    <inicio/>
+  </section>
+</template>
+
+<script>
+
+import inicio from "./components/inicio"
+
+export default {
+ components: {inicio,},
+}
+</script>
+
+<style>
+
+</style>
