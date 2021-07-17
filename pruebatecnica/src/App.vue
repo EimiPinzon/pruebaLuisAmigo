925f767220a85d09e90ef85614064ad9012072d0
Eimi <eimipinzonhoyos@gmail.com>
2021-07-17 18:16:43 -0500

    actualizacion

--- pruebatecnica/src/App.vue
+++ pruebatecnica/src/App.vue
@@ -1,16 +1,42 @@
 <template>
-  <section>
-    <h1>prueba tecnica</h1>
-    <inicio/>
-  </section>
+  <div>
+    <table>
+      <tbody>
+        <tr>
+          <td>celda</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-
-import inicio from "./components/inicio"
+import axios from 'axios'
 
 export default {
- components: {inicio,},
+ data(){
+   return{
+     todos:null
+     }
+ },
+ mounted(){
+   console.log('hola mundooo')
+   this.getTodos();
+ },
+ methods: {
+   getTodos(){
+     console.log('aca va el codigo de get todos.')
+     axios
+      .get('https://aws.random.cat/meow')
+      .then(response =>{
+          console.log(response)
+          this.todos = response.data
+      })
+      .catch(e=> console.log(e))
+   }
+ }
+
+ 
 }
 </script>
 
---
+++ pruebatecnica/src/components/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+      <div class="row">
+          <div class="col-md-6">
+              <h1>hola, uso bootstrap</h1>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
