6d95b058c1d23608ba237f229f49116a08d4882f
Eimi <eimipinzonhoyos@gmail.com>
2021-07-18 16:56:13 -0500

    actualización

--- pruebatecnica/src/App.vue
+++ pruebatecnica/src/App.vue
@@ -1,23 +1,17 @@
 <template>
   <section>
-
-      <div class="container">
-      <div class="row">
-        <div class="col-md12">
-          <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-md">
-          <a class="navbar-brand" href="#">Navbar</a>
-        </div>
-      </nav>
-        </div>
-      </div> <br>
     
-      <div class="row"> 
-        <div class="col-md-12">
+    <div class="container">
+      <div class="row">
+
+        <img  alt="logo" src="./assets/makeup.jpg">
+        
+        <div class="col-md-12"><br><br>
+
           <table class="table table-bordered">
             <thead>
-              <tr>
-                <th scope="col"></th>
+              <tr style="background-color: #d1e7dd;">
+                <th scope="col">ID PRODUCTO</th>
                 <th scope="col">PRODUCTO</th>
                 <th scope="col">TIPO</th>
                 <th scope="col">ETIQUETA</th>
@@ -26,51 +20,52 @@
             </thead>
             <tbody>
               <tr v-for="todo in todos" :key="todo.id">
-                <th scope="row"></th>
+                <th scope="row">{{ todo.id }}</th>
                 <td>{{ todo.category }}</td>
-                <td>{{ todo.product_type  }}</td>
+                <td>{{ todo.product_type }}</td>
                 <td>{{ todo.tag_list[0] }}</td>
                 <td>{{ todo.created_at }}</td>
               </tr>
             </tbody>
           </table>
+
         </div>
       </div>
     </div>
-
   </section>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 
 export default {
- data(){
-   return{
-     todos:null
-     }
- },
- mounted(){
-   console.log('hola mundooo')
-   this.getTodos();
- },
- methods: {
-   getTodos(){
-     console.log('aca va el codigo de get todos.')
-     axios
-      .get('http://makeup-api.herokuapp.com/api/v1/products.json')
-      .then(response =>{
-          console.log(response)
-          this.todos = response.data
-      })
-      .catch(e=> console.log(e))
-   }
- },
-
- 
+  
+  data() {
+    return {
+      todos: null,
+    };
+  },
+  mounted() {
+    console.log("hola mundooo");
+    this.getTodos();
+  },
+  methods: {
+    getTodos() {
+      console.log("aca va el codigo de get todos.");
+      axios
+        .get("http://makeup-api.herokuapp.com/api/v1/products.json")
+        .then((response) => {
+          console.log(response);
+          this.todos = response.data;
+        })
+        .catch((e) => console.log(e));
+    },
+  },
+  
 }
+
+
 </script>
 
-<style>
-
+<style lang="css">
 </style>
