db5fa0efb26ff623ef09d7f3ffae7d2ffdc5da73
Eimi <eimipinzonhoyos@gmail.com>
2021-07-17 22:28:14 -0500

    API

--- pruebatecnica/src/App.vue
+++ pruebatecnica/src/App.vue
@@ -1,13 +1,44 @@
 <template>
-  <div>
-    <table>
-      <tbody>
-        <tr>
-          <td>celda</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+  <section>
+
+      <div class="container">
+      <div class="row">
+        <div class="col-md12">
+          <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-md">
+          <a class="navbar-brand" href="#">Navbar</a>
+        </div>
+      </nav>
+        </div>
+      </div> <br>
+    
+      <div class="row"> 
+        <div class="col-md-12">
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th scope="col"></th>
+                <th scope="col">PRODUCTO</th>
+                <th scope="col">TIPO</th>
+                <th scope="col">ETIQUETA</th>
+                <th scope="col">FECHA DE CREACIÓN</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="todo in todos" :key="todo.id">
+                <th scope="row"></th>
+                <td>{{ todo.category }}</td>
+                <td>{{ todo.product_type  }}</td>
+                <td>{{ todo.tag_list[0] }}</td>
+                <td>{{ todo.created_at }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+
+  </section>
 </template>
 
 <script>
@@ -27,14 +58,14 @@ export default {
    getTodos(){
      console.log('aca va el codigo de get todos.')
      axios
-      .get('https://aws.random.cat/meow')
+      .get('http://makeup-api.herokuapp.com/api/v1/products.json')
       .then(response =>{
           console.log(response)
           this.todos = response.data
       })
       .catch(e=> console.log(e))
    }
- }
+ },
 
  
 }
--- pruebatecnica/src/components/index.vue
+++ pruebatecnica/src/components/index.vue
@@ -1,17 +1,22 @@
 <template>
-  <div class="container">
-      <div class="row">
-          <div class="col-md-6">
-              <h1>hola, uso bootstrap</h1>
-          </div>
-      </div>
+  <div>
+    <b-table striped hover :items="items"></b-table>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+  export default {
+    data() {
+      return {
+        items: [
+          { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+          { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+          { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+          { age: 38, first_name: 'Jami', last_name: 'Carney' }
+        ]
+      }
+    }
+  }
 </script>
 
 <style>
